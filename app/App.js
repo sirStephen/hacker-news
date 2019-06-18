73b80dd1ac9328a49ae4687def6f7a54c22e231c
sirStephen <itobitoast@gmail.com>
2019-06-18 21:42:46 +0100

    change heading

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render() {
         return(
             <div>
-                <h1>Hey world</h1>
+                <h1>Hey</h1>
             </div>
         )
     }
